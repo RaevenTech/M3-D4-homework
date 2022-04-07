0c0c67c0d0beec96790d4d3a9e0a1c080a874f0f
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-07 15:53:06 +0100

    books page

---
+++ script.js
@@ -0,0 +1,81 @@
+let books = [];
+let booksInCartList = [];
+
+const row = document.querySelector(".row");
+const cart = document.querySelector(".cart-list");
+
+window.onload = () => {
+    fetch("https://striveschool-api.herokuapp.com/books")
+        .then((response) => response.json())
+        .then((receivedBooks) => {
+            books = receivedBooks;
+            displayBooks(receivedBooks);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+function displayBooks(books) {
+    row.innerHTML = books
+        .map(
+            (book, i) => `
+                    <div class="card">
+                        <div class="img-container">
+                            <img src="${book.img}" class="card-img-top" alt="..." />
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title" onClick="bookDetails(event, ${i})" data-toggle="modal" data-target="#exampleModal">${book.title}</h5>
+                            <p class="card-text">
+                                Price: ${book.price}
+                            </p>
+                            <a href="#" class="btn btn-dark btnAdd" onClick="btnAdd(event, ${i})">I Take It</a>
+                            <a href="#" class="btn btn-secondary btnEdit" onClick="btnSkip(event)">Nope</a>
+                        </div>
+                    </div>
+            `
+        )
+        .join("");
+}
+
+const btnSkip = (event) => {
+    event.target.closest(".card").remove();
+};
+
+const btnAdd = (event, i) => {
+    const currentCard = event.target.closest(".card");
+    currentCard.classList.add("book-in-cart");
+    booksInCartList.push(books[i]);
+    bookToBuy(booksInCartList);
+};
+
+const bookToBuy = (books) => {
+    cart.innerHTML = books
+        .map(
+            (book, i) => `
+                    <div class="card card-cart">
+                    <div class="img-container">
+                    <img src="${book.img}" class="card-img-top" alt="..." />
+                </div>
+                        <div class="card-body">
+                            <h5 class="card-title">${book.title}</h5>
+                            <p class="card-text">
+                                Price: ${book.price}
+                            </p>
+                            <a href="#" class="btn btn-secondary" onClick="btnDeleteBookCart(event)">remove</a>
+                        </div>
+                    </div>
+            `
+        )
+        .join("");
+};
+
+const btnDeleteBookCart = (event) => {
+    event.target.closest(".card").remove();
+    event.target.closest(".card").innerHTML;
+};
+
+const removeAll = () => {
+    cart.innerHTML = "";
+    booksInCartList = [];
+};
